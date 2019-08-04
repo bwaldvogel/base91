f197d8fc0944002b4d2d414674cf5d12c2416fe7
Benedikt Waldvogel <mail@bwaldvogel.de>
2019-08-04 11:04:35 +0200

    Reformat code

--- src/main/java/de/bwaldvogel/base91/Base91.java
+++ src/main/java/de/bwaldvogel/base91/Base91.java
@@ -5,14 +5,14 @@ import java.io.ByteArrayOutputStream;
 /**
  * Modified version of Jochaim Henke's original code from
  * http://base91.sourceforge.net/
- *
+ * <p>
  * basE91 encoding/decoding routines
- *
+ * <p>
  * Copyright (c) 2000-2006 Joachim Henke All rights reserved.
- *
+ * <p>
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * <p>
  * - Redistributions of source code must retain the above copyright notice, this
  * list of conditions and the following disclaimer. - Redistributions in binary
  * form must reproduce the above copyright notice, this list of conditions and
@@ -20,7 +20,7 @@ import java.io.ByteArrayOutputStream;
  * with the distribution. - Neither the name of Joachim Henke nor the names of
  * his contributors may be used to endorse or promote products derived from this
  * software without specific prior written permission.
- *
+ * <p>
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -38,94 +38,90 @@ import java.io.ByteArrayOutputStream;
  */
 public class Base91 {
 
-    public static final byte[] ENCODING_TABLE;
+    private static final byte[] ENCODING_TABLE;
     private static final byte[] DECODING_TABLE;
     private static final int BASE;
     private static final float AVERAGE_ENCODING_RATIO = 1.2297f;
 
     static {
-	String ts = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~\"";
-	ENCODING_TABLE = ts.getBytes();
-	BASE = ENCODING_TABLE.length;
+        String ts = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~\"";
+        ENCODING_TABLE = ts.getBytes();
+        BASE = ENCODING_TABLE.length;
 
-	DECODING_TABLE = new byte[256];
-	for (int i = 0; i < 256; ++i)
-	    DECODING_TABLE[i] = -1;
+        DECODING_TABLE = new byte[256];
+        for (int i = 0; i < 256; ++i)
+            DECODING_TABLE[i] = -1;
 
-	for (int i = 0; i < BASE; ++i)
-	    DECODING_TABLE[ENCODING_TABLE[i]] = (byte) i;
+        for (int i = 0; i < BASE; ++i)
+            DECODING_TABLE[ENCODING_TABLE[i]] = (byte) i;
     }
 
     public static byte[] encode(byte[] data) {
 
-	int estimatedSize = (int) Math.ceil(data.length * AVERAGE_ENCODING_RATIO);
-	ByteArrayOutputStream output = new ByteArrayOutputStream(estimatedSize);
+        int estimatedSize = (int) Math.ceil(data.length * AVERAGE_ENCODING_RATIO);
+        ByteArrayOutputStream output = new ByteArrayOutputStream(estimatedSize);
 
-	int ebq = 0;
-	int en = 0;
-	for (int i = 0; i < data.length; ++i) {
-	    ebq |= (data[i] & 255) << en;
-	    en += 8;
-	    if (en > 13) {
-		int ev = ebq & 8191;
+        int ebq = 0;
+        int en = 0;
+        for (int i = 0; i < data.length; ++i) {
+            ebq |= (data[i] & 255) << en;
+            en += 8;
+            if (en > 13) {
+                int ev = ebq & 8191;
 
-		if (ev > 88) {
-		    ebq >>= 13;
-		    en -= 13;
-		} else {
-		    ev = ebq & 16383;
-		    ebq >>= 14;
-		    en -= 14;
-		}
-		output.write(ENCODING_TABLE[ev % BASE]);
-		output.write(ENCODING_TABLE[ev / BASE]);
-	    }
-	}
+                if (ev > 88) {
+                    ebq >>= 13;
+                    en -= 13;
+                } else {
+                    ev = ebq & 16383;
+                    ebq >>= 14;
+                    en -= 14;
+                }
+                output.write(ENCODING_TABLE[ev % BASE]);
+                output.write(ENCODING_TABLE[ev / BASE]);
+            }
+        }
 
-	if (en > 0) {
-	    output.write(ENCODING_TABLE[ebq % BASE]);
-	    if (en > 7 || ebq > 90) {
-		output.write(ENCODING_TABLE[ebq / BASE]);
-	    }
-	}
+        if (en > 0) {
+            output.write(ENCODING_TABLE[ebq % BASE]);
+            if (en > 7 || ebq > 90) {
+                output.write(ENCODING_TABLE[ebq / BASE]);
+            }
+        }
 
-	return output.toByteArray();
+        return output.toByteArray();
     }
 
     public static byte[] decode(byte[] data) {
+        int dbq = 0;
+        int dn = 0;
+        int dv = -1;
 
-	// if (data.length == 0)
-	// return new byte[] {};
+        int estimatedSize = Math.round(data.length / AVERAGE_ENCODING_RATIO);
+        ByteArrayOutputStream output = new ByteArrayOutputStream(estimatedSize);
 
-	int dbq = 0;
-	int dn = 0;
-	int dv = -1;
+        for (int i = 0; i < data.length; ++i) {
+            if (DECODING_TABLE[data[i]] == -1)
+                continue;
+            if (dv == -1)
+                dv = DECODING_TABLE[data[i]];
+            else {
+                dv += DECODING_TABLE[data[i]] * BASE;
+                dbq |= dv << dn;
+                dn += (dv & 8191) > 88 ? 13 : 14;
+                do {
+                    output.write((byte) dbq);
+                    dbq >>= 8;
+                    dn -= 8;
+                } while (dn > 7);
+                dv = -1;
+            }
+        }
 
-	int estimatedSize = Math.round(data.length / AVERAGE_ENCODING_RATIO);
-	ByteArrayOutputStream output = new ByteArrayOutputStream(estimatedSize);
+        if (dv != -1) {
+            output.write((byte) (dbq | dv << dn));
+        }
 
-	for (int i = 0; i < data.length; ++i) {
-	    if (DECODING_TABLE[data[i]] == -1)
-		continue;
-	    if (dv == -1)
-		dv = DECODING_TABLE[data[i]];
-	    else {
-		dv += DECODING_TABLE[data[i]] * BASE;
-		dbq |= dv << dn;
-		dn += (dv & 8191) > 88 ? 13 : 14;
-		do {
-		    output.write((byte) dbq);
-		    dbq >>= 8;
-		    dn -= 8;
-		} while (dn > 7);
-		dv = -1;
-	    }
-	}
-
-	if (dv != -1) {
-	    output.write((byte) (dbq | dv << dn));
-	}
-
-	return output.toByteArray();
+        return output.toByteArray();
     }
 }
--- src/test/java/de/bwaldvogel/base91/Base91Test.java
+++ src/test/java/de/bwaldvogel/base91/Base91Test.java
@@ -1,11 +1,10 @@
 package de.bwaldvogel.base91;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.nio.charset.Charset;
-import java.util.HashMap;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
@@ -14,62 +13,60 @@ import org.junit.Test;
 
 public class Base91Test {
 
-    private static Charset CHARSET = Charset.forName("ISO-8859-1");
-    private static double worstCaseRatio = 1.2308;
-    private static double bestCaseRatio = 1.1429;
+	private static final Charset CHARSET = StandardCharsets.UTF_8;
+	private static final double worstCaseRatio = 1.2308;
+	private static final double bestCaseRatio = 1.1429;
 
-    @Test
-    public void testEncodeDecode() throws Exception {
+	@Test
+	public void testEncodeDecode() throws Exception {
+		Map<String, String> encodeDecodes = new LinkedHashMap<>();
+		encodeDecodes.put("test", "fPNKd");
+		encodeDecodes.put("Never odd or even\n", "_O^gp@J`7RztjblLA#_1eHA");
+		encodeDecodes.put("May a moody baby doom a yam?\n", "8D9Kc)=/2$WzeFui#G9Km+<{VT2u9MZil}[A");
+		encodeDecodes.put("", "");
+		encodeDecodes.put("a", "GB");
 
-	Map<String, String> encodeDecodes = new HashMap<String, String>();
-	encodeDecodes.put("test", "fPNKd");
-	encodeDecodes.put("Never odd or even\n", "_O^gp@J`7RztjblLA#_1eHA");
-	encodeDecodes.put("May a moody baby doom a yam?\n", "8D9Kc)=/2$WzeFui#G9Km+<{VT2u9MZil}[A");
-	encodeDecodes.put("", "");
-	encodeDecodes.put("a", "GB");
-
-	for (Entry<String, String> entry : encodeDecodes.entrySet()) {
-	    String plainText = entry.getKey();
-	    String encodedText = entry.getValue();
-	    byte[] encode = Base91.encode(plainText.getBytes(CHARSET));
-	    byte[] decode = Base91.decode(encode);
-	    assertEquals(encodedText, new String(encode, CHARSET));
-	    assertEquals(plainText, new String(decode, CHARSET));
-	}
-    }
-
-    @Test
-    public void testRandomEncodeDecode() throws Exception {
-
-	Random random = new Random(System.currentTimeMillis());
-
-	int encodedSize = 0;
-	int plainSize = 0;
-
-	double worstEncodingRatio = Double.MIN_VALUE;
-	double bestEncodingRatio = Double.MAX_VALUE;
-
-	for (int i = 0; i < 10000; i++) {
-	    byte[] bytes = new byte[random.nextInt(1000) + 100];
-	    random.nextBytes(bytes);
-	    byte[] encode = Base91.encode(bytes);
-	    byte[] decode = Base91.decode(encode);
-
-	    assertArrayEquals(decode, bytes);
-
-	    plainSize += bytes.length;
-	    encodedSize += encode.length;
-
-	    double encodingRatio = (double) encode.length / bytes.length;
-	    worstEncodingRatio = Math.max(worstEncodingRatio, encodingRatio);
-	    bestEncodingRatio = Math.min(bestEncodingRatio, encodingRatio);
+		for (Entry<String, String> entry : encodeDecodes.entrySet()) {
+			String plainText = entry.getKey();
+			String encodedText = entry.getValue();
+			byte[] encode = Base91.encode(plainText.getBytes(CHARSET));
+			byte[] decode = Base91.decode(encode);
+			assertEquals(encodedText, new String(encode, CHARSET));
+			assertEquals(plainText, new String(decode, CHARSET));
+		}
 	}
 
-	double encodingRatio = (double) encodedSize / plainSize;
-	assertTrue(encodingRatio <= worstCaseRatio);
-	assertTrue(encodingRatio >= bestCaseRatio);
-	System.out.println("encoding ratio: " + encodingRatio);
-	System.out.println("worst encoding ratio: " + worstEncodingRatio);
-	System.out.println("best encoding ratio: " + bestEncodingRatio);
-    }
+	@Test
+	public void testRandomEncodeDecode() throws Exception {
+		Random random = new Random(System.currentTimeMillis());
+
+		int encodedSize = 0;
+		int plainSize = 0;
+
+		double worstEncodingRatio = Double.MIN_VALUE;
+		double bestEncodingRatio = Double.MAX_VALUE;
+
+		for (int i = 0; i < 10000; i++) {
+			byte[] bytes = new byte[random.nextInt(1000) + 100];
+			random.nextBytes(bytes);
+			byte[] encode = Base91.encode(bytes);
+			byte[] decode = Base91.decode(encode);
+
+			assertArrayEquals(decode, bytes);
+
+			plainSize += bytes.length;
+			encodedSize += encode.length;
+
+			double encodingRatio = (double) encode.length / bytes.length;
+			worstEncodingRatio = Math.max(worstEncodingRatio, encodingRatio);
+			bestEncodingRatio = Math.min(bestEncodingRatio, encodingRatio);
+		}
+
+		double encodingRatio = (double) encodedSize / plainSize;
+		assertTrue(encodingRatio <= worstCaseRatio);
+		assertTrue(encodingRatio >= bestCaseRatio);
+		System.out.println("encoding ratio: " + encodingRatio);
+		System.out.println("worst encoding ratio: " + worstEncodingRatio);
+		System.out.println("best encoding ratio: " + bestEncodingRatio);
+	}
 }
